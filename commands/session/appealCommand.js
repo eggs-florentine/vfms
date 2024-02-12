bff0e8dbe66909707d390c701cc416e80bf0cbd1
eggs-florentine <139823988+eggs-florentine@users.noreply.github.com>
2024-02-12 20:37:41 +1300

    feat: add subcommands, DM functionality to /appeal

--- commands/session/appealCommand.js
+++ commands/session/appealCommand.js
@@ -4,7 +4,16 @@ const fs = require('node:fs');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('appeal')
-    .setDescription('make a decision on am appeal')
+    .setDescription('make a decision on an appeal')
+    .addSubcommand(subcommand =>
+		subcommand
+			.setName('accept')
+			.setDescription('accept an appeal'),)
+    .addSubcommand(subcommand =>
+            subcommand
+                .setName('deny')
+                .setDescription('deny an appeal')
+                .addStringOption(option => option.setName('notes').setDescription('notes e.g reason for denial or banned from appealing')))
     .addUserOption(option =>
       option
         .setName('user')
@@ -12,23 +21,35 @@ module.exports = {
         .setRequired(true)),
   async execute(interaction) {
 
-    const accept = new EmbedBuilder()
-    .setTitle('<:vfms:1153668810814013530> Notification of Appeal Acceptance')
-    .setDescription('Hello (user). Your recent appeal against your VFMS permanent suspension has been **accepted**. Welcome back to the team! Please take the following measures.')
-    .setColor(0x0faf4f)
-    .addFields(
-        { name: 'Review the rules, regulations and guidelines', value: 'After appealing your removal, you are held to an even higher standard than our regular members for a certain period of time after your appeal. We strongly encourage you review the guidelines again to prevent Disciplinary Action.'},
-        { name: 'Be patient and await your re-entry into the team', value: 'A member of the Senior Leadership Team will contact you once your suspension is removed and you can rejoin the team.'},
-        { name: 'Customer', value: 'After your suspension, you were kicked from the Roblox group. Once a member of the Senior Leadership Team tells you you have been unsuspended, you need to rejoin the roblox group and wait a bit to play on VFMS again!'},
-    )
+    if (interaction.options.getSubcommand() === 'accept') { 
+        
+        const accept = new EmbedBuilder()
+        .setTitle('<:vfms:1153668810814013530> Notification of Appeal Acceptance')
+        .setDescription('Hello (user). Your recent appeal against your VFMS permanent suspension has been **accepted**. Welcome back to the team! Please take the following measures.')
+        .setColor(0x0faf4f)
+        .addFields(
+            { name: 'Review the rules, regulations and guidelines', value: 'After appealing your removal, you are held to an even higher standard than our regular members for a certain period of time after your appeal. We strongly encourage you review the guidelines again to prevent Disciplinary Action.'},
+            { name: 'Be patient and await your re-entry into the team', value: 'A member of the Senior Leadership Team will contact you once your suspension is removed and you can rejoin the team.'},
+            { name: 'Rejoin the VFMS group and begin playing', value: 'After your suspension, you were kicked from the Roblox group. Once a member of the Senior Leadership Team tells you you have been unsuspended, you need to rejoin the roblox group and wait a bit to play on VFMS again!'},
+        )
 
-    const deny = new EmbedBuilder()
-    .setTitle('<:vfms:1153668810814013530> Notification of Appeal Denial')
-    .setDescription('Hello (user). Your recent appeal against your VFMS permanent suspension has been **denied**. More information on your denial and notes  the reviewing staff member gave you are available below.')
-    .setColor(0x0faf4f)
-    .addFields(
-        { name: 'Notes', value: 'notes'},
-    )
+        interaction.otpions.getOption('user').send({embeds: [accept]});
+    }
+
+    if (interaction.options.getSubcommand() === 'deny') { 
+        
+        const deny = new EmbedBuilder()
+        .setTitle('<:vfms:1153668810814013530> Notification of Appeal Denial')
+        .setDescription('Hello (user). Your recent appeal against your VFMS permanent suspension has been **denied**. More information on your denial and notes  the reviewing staff member gave you are available below.')
+        .setColor(0x0faf4f)
+        .addFields(
+            { name: 'Notes', value: 'notes'},
+        )
+
+        interaction.otpions.getOption('user').send({embeds: [deny]});
+    }
+
+    
 
     await interaction.reply({ content: 'Decision issued!', ephemeral: true });
   },
